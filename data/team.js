e9ec144b262b1b6e2cdda7e009eddc254ff7a3cd
Chris Schimetschka <chris.schimetschka@gmail.com>
2022-10-23 22:39:48 +0200

    slight changes

--- data/team.js
+++ data/team.js
@@ -1,46 +1,46 @@
 const team = [
-    {
-        id: "mark-gutjahr",
-        img: "./assets/Mark.png",
-        title: "Head of Design",
-        name: "Mark Gutjahr",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis."
-    },
-    {
-        id: "michaela-finkenzeller",
-        img: "./assets/Michaela.png",
-        title: "Designer",
-        name: "Michaela Finkenzeller",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis."
-    },
-    {
-        id: "benjamin-lanfer",
-        img: "./assets/Benji.png",
-        title: "Color Scientist",
-        name: "Benjamin Lanfer",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis."
-    },
-    {
-        id: "jens-wegner",
-        img: "./assets/Jens.png",
-        title: "Head of Digital Business Solutions",
-        name: "Jens Wegner",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis."
-    },
-    {
-        id: "florina-trost",
-        img: "./assets/Flowrina.png",
-        title: "Designer",
-        name: "Florina Trost",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis."
-    },
-    {
-        id: "chris-schimetschka",
-        img: "./assets/Chrizzle.png",
-        title: "Product Manager",
-        name: "Christian Schimetschka",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis?"
-    },
+	{
+		id: 'mark-gutjahr',
+		img: './assets/Mark.png',
+		title: 'Head of Design',
+		name: 'Mark Gutjahr',
+		desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis.',
+	},
+	{
+		id: 'michaela-finkenzeller',
+		img: './assets/Michaela.png',
+		title: 'Designer',
+		name: 'Michaela Finkenzeller',
+		desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis.',
+	},
+	{
+		id: 'benjamin-lanfer',
+		img: './assets/Benji.png',
+		title: 'Color Scientist',
+		name: 'Benjamin Lanfer',
+		desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis.',
+	},
+	{
+		id: 'jens-wegner',
+		img: './assets/Jens.png',
+		title: 'Head of Digital Business Solutions',
+		name: 'Jens Wegner',
+		desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis.',
+	},
+	{
+		id: 'florina-trost',
+		img: './assets/Flowrina.png',
+		title: 'Designer',
+		name: 'Florina Trost',
+		desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis.',
+	},
+	{
+		id: 'chris-schimetschka',
+		img: './assets/Chrizzle.png',
+		title: 'Software Developer',
+		name: 'Christian Schimetschka',
+		desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus optio dolorem eius assumenda, cupiditate dicta maiores iste deleniti hic repellat vero, dignissimos necessitatibus debitis?',
+	},
 ];
 
-export default team
+export default team;
